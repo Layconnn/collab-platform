47e630fa8c6c74ff3dcce9c53ed5485664b4b6f4
Jinadu Olamilkean <olamilekanjinadu21@gmail.com>
2026-03-04 13:19:52 +0100

    fix

--- apps/web/app/api/trpc/[trpc]/route.ts
+++ apps/web/app/api/trpc/[trpc]/route.ts
@@ -1,7 +1,7 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
-import { appRouter } from "@/server/api/root";
-import { createTRPCContext } from "@/server/api/trpc";
+import { appRouter } from "../../../../server/api/root";
+import { createTRPCContext } from "../../../../server/api/trpc";
 
 const handler = (req: Request) =>
   fetchRequestHandler({
